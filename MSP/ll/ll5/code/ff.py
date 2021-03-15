39636e4a7fdaec263a4e760ec4fbecc306895c48
Catalin <catalinhimself@gmail.com>
2021-03-15 21:50:09 +0200

    123

--- MSP/ll/ll5/code/ff.py
+++ MSP/ll/ll5/code/ff.py
@@ -1,25 +1,13 @@
-import json
 from collections import defaultdict
-import numpy as np
 
-g = json.load(open("1.json"))
-temp = defaultdict(list)
-for key in [*g]:
-    for v in g[key]:
-        temp[int(key)].append(v)
-g = temp
-c = defaultdict(int)
-temp = json.load(open("1.c.json"))
-for k in [*temp]:
-    for l in temp[k]:
-        c[(int(k), l[0])] = l[1]
+g = {1: [2, 3, 5], 2: [4, 5], 3: [6], 4: [3, 8],
+     5: [6, 7], 6: [7], 7: [4, 9], 8: [7, 9], 9: []}
+
+c = {(1, 2): 8, (1, 3): 9, (1, 5): 6, (2, 4): 5, (2, 5): 6, (3, 6): 6, (4, 3): 5, (4, 8)     : 4, (5, 6): 3, (5, 7): 5, (6, 7): 4, (7, 4): 3, (7, 9): 9, (8, 7): 5, (8, 9): 6}
+
 f = defaultdict(list)
 for k in [*c]:
     f[k].append(0)
-for k in [*g]:
-    for v in g[k]:
-        if v not in [*g]:
-            g[v] = []
 print("graf")
 print(g)
 print("------------------------------------")
@@ -29,64 +17,3 @@ print("------------------------------------")
 print("flux")
 print(f)
 print("--------------------------------------")
-intrari = defaultdict(bool)
-for k in [*g]:
-    intrari[k] = False
-for k in [*g]:
-    if not len(g[k]):
-        destinatia = k
-    for v in g[k]:
-        intrari[v] = True
-for v in [*intrari]:
-    if not intrari[v]:
-        sursa = v
-gr = np.zeros((destinatia+1, destinatia+1))
-for k in [*g]:
-    for v in g[k]:
-        gr[k][v] = c[(k, v)]
-print(gr)
-
-
-def BFS(graph, s, t, parent):
-    # Return True if there is node that has not iterated.
-    visited = [False] * len(graph)
-    queue = []
-    queue.append(s)
-    visited[s] = True
-
-    while queue:
-        u = queue.pop(0)
-        for ind in range(len(graph[u])):
-            if visited[ind] is False and graph[u][ind] > 0:
-                queue.append(ind)
-                visited[ind] = True
-                parent[ind] = u
-
-    return True if visited[t] else False
-
-
-def FordFulkerson(graph, source, sink):
-    # This array is filled by BFS and to store path
-    parent = [-1] * (len(graph))
-    max_flow = 0
-    while BFS(graph, source, sink, parent):
-        path_flow = float("Inf")
-        s = sink
-
-        while s != source:
-            # Find the minimum value in select path
-            path_flow = min(path_flow, graph[parent[s]][s])
-            s = parent[s]
-
-        max_flow += path_flow
-        v = sink
-
-        while v != source:
-            u = parent[v]
-            graph[u][v] -= path_flow
-            graph[v][u] += path_flow
-            v = parent[v]
-    return max_flow
-
-
-print(FordFulkerson(gr, sursa, destinatia))
