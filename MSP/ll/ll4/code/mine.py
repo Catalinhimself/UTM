b684d585b3386e097f707a5ea60111c3262d48ef
catalin <catalinhimself@gmail.com>
2021-03-25 19:10:56 +0200

    msp lab 4 fixed, + examples

--- MSP/ll/ll4/code/mine.py
+++ MSP/ll/ll4/code/mine.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
-import networkx as nx
-import matplotlib.pyplot as plt
+try:
+    import networkx as nx
+    import matplotlib.pyplot as plt
+    visual_libs = True
+except ImportError:
+    visual_libs = False
 import json
-
-# pentru coada si stiva voi folosi lista pop append si voi sterge elementu 1 cand am nevoie de el
 
 
 class GRAF:
@@ -42,71 +44,67 @@ class GRAF:
     def afiseazaLista(self):
         print("lista de adiacenta")
         for k in [*self.graf]:
-            print(k, "-", end=" ")
+            print(k, "|", end=" ")
             for v in self.graf[k]:
-                print(v, ",", end="")
+                print(v, end="_ ")
             print("0")
 
     def graf_de_acoperire(self, start):
         vizitat = set()
-        # pasul 1
-
+        # pasul 1 | fa1 si fa2 vide
         FA1 = []
         FA2 = []
         # de la pasul 10
         while [*self.graf] != list(vizitat):
-            # pasul 2
+            # pasul 2 alegem varful initial
             if start not in vizitat:
                 FA1.append(start)
             else:
                 for v in [*self.graf]:
                     if v not in vizitat:
                         FA1.append(v)
+                        break
+            # pasul 8 => 10 daca fa1 este vid
             while FA1:
-                # pasul 3
+                # pasul 3 => 8 daca fa1 este vid| p primul element extras din fa1
                 while FA1:
                     p = FA1.pop(0)
                     vizitat.add(p)
-                    # pasul 4
+                    # pasul 4 | adaugam fii nevizitati ai lui p in fa2
                     if self.graf[p]:
                         for v in self.graf[p]:
                             if v not in vizitat:
                                 FA2.append(v)
-                        # pasul 5
+                        # pasul 5 | eliminam muchiiile legate intre ele din fa2
                         for a in FA2:
                             for b in FA2:
                                 if b in self.graf[a]:
                                     self.graf[a].remove(b)
-
-                        # pasul 6
-                        nr = 0
+                        # pasul 6 | eliminam toate toate-1 muchii care laga fa1 si fa2
                         for a in FA2:
                             for b in FA1:
                                 if b in self.graf[a]:
-                                    nr += 1
-                        for a in FA2:
-                            for b in FA1:
-                                if nr:
-                                    if b in self.graf[a]:
-                                        self.graf[a].remove(b)
-                                        nr -= 1
-                                    if a in self.graf[b]:
-                                        self.graf[b].remove(a)
-                                        nr -= 1
+                                    self.graf[a].remove(b)
+                                if a in self.graf[b]:
+                                    self.graf[b].remove(a)
                         # pasul 7 => continuarea ciclului
-                    # pasul 8
-                    temp = FA1
-                    FA1 = FA2
-                    FA2 = temp
-                    # pasul 9 => repetam 3 - 8
-                # pasul 10 se trece la 2 daca nu toate v au fost vizitate
+            # pasul 8
+                temp = FA1
+                FA1 = FA2
+                FA2 = temp
+                # pasul 9 => repetam 3 - 8
+        # pasul 10 => 2 daca nu avem toate varfurile vizitate
 
     def salveaza(self):
-        json.dump(self.graf, open("a.json", 'w'))
+        f = input("dati denumirea fisierului ( fara extensie ): ")
+        json.dump(self.graf, open(f+".json", 'w'))
+        print("fisierul salvat cu succes")
 
     def impota(self):
-        self.graf = json.load(open("a.json"))
+        f = input("dati denumirea fisierului ( fara extensie ): ")
+        self.graf = json.load(open(f+".json"))
         self.graf = {int(k): [int(i) for i in v] for k, v in self.graf.items()}
+        print("fisierul importa cu succes")
 
     def deseneazaGraful(self):
         g = nx.DiGraph()
@@ -128,39 +126,29 @@ class GRAF:
             print("")
             print("( 1 ) - pentru a citi de la tastatura lista de adiacenta")
             print("( 2 ) - pentru a afisa lista")
-            print("( 3 ) - pentru a afisa forma grafica")
+            if visual_libs:
+                print("( 3 ) - pentru a afisa forma grafica")
             print("( 4 ) - pentru a efectua genera graful de acoperire")
-            print("( 9 ) - pentru a adauga varfurile predefinite ( de profesoara )")
             o = input()
             if o == "q":
                 break
             elif o == "c":
                 self.impota()
-                print("importarea efectuata cu succes")
             elif o == "1":
                 self.citirea()
             elif o == "s":
                 self.salveaza()
-                print("salvarea efectuata cu succes")
             elif o == "2":
                 self.afiseazaLista()
             elif o == "3":
-                self.deseneazaGraful()
+                if visual_libs:
+                    self.deseneazaGraful()
             elif o == "4":
-                print(self.graf)
-                print("dati varful pentru care doriti sa efectuati cautarea in latime")
-                v = input()
-                self.graf_de_acoperire(int(v))
-            elif o == "9":
-                self.adaugaArc(1, 2)
-                self.adaugaArc(1, 3)
-                self.adaugaArc(2, 4)
-                self.adaugaArc(2, 5)
-                self.adaugaArc(3, 6)
-                self.afiseazaLista()
-                self.graf_de_acoperire(1)
-                self.parcugereaAdancime(1)
-                self.deseneazaGraful()
+                v = int(input("radacina grafuli de acoperire este: "))
+                if v not in self.graf.keys():
+                    print("radacina invalida, se va lua radacina 1")
+                    v = 1
+                self.graf_de_acoperire(v)
 
 
 graf = GRAF()
