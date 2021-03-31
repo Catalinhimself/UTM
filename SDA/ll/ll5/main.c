cf19d8734307cb84f65c7f185dd458f37632ee31
Catalin <catalinhimself@gmail.com>
2021-03-31 23:42:56 +0300

    lab12 fiz

--- SDA/ll/ll5/main.c
+++ SDA/ll/ll5/main.c
@@ -143,7 +143,6 @@ list **divide_in_n_lists_b(list *head, int n)
     int nodes = count_nodes(head);
     int count[n];
     int remainder = nodes - (nodes / n) * n;
-    printf("%d %d", remainder, nodes / n);
     for (int i = 0; i < n; i++)
     {
         count[i] = nodes / n;
@@ -178,44 +177,91 @@ list **divide_in_n_lists_b(list *head, int n)
 
     return liste;
 }
+int menu()
+{
+    printf("__Program_la_SDA__\n");
+    printf("1. Cititi o lista de la tastatura\n");
+    printf("2. Generati o lista aleatoare\n");
+    int i;
+    scanf("%d", &i);
+    return i;
+}
+int get_num(int n)
+{
+    printf("dati elementul %d: ", n);
+    scanf("%d", &n);
+    return n;
+}
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
-    int range = random_range(20, 50);
-    printf("%d\n", range);
-    list *head = forge_node(random_range(-1000, 1000));
+
+    int option = menu();
+    printf("dati numarul de elemente ale listei\n");
+    int range;
+    scanf("%d", &range);
+    int node;
+    int z = 1;
+    if (option == 1)
+        node = get_num(z);
+    if (option == 2)
+        node = random_range(-1000, 1000);
+    list *head = forge_node(node);
     list *t = head;
     for (int i = 1; i < range; i++)
     {
-        t->next = forge_node(random_range(-1000, 1000));
+        z++;
+        if (option == 1)
+            node = get_num(z);
+        if (option == 2)
+            node = random_range(-1000, 1000);
+        t->next = forge_node(node);
         t = t->next;
     }
+    printf("Lista citita:\n");
     pirnt_list(head);
-    printf("total : %d nodes", count_nodes(head));
+    printf("contine : %d elemente\n", count_nodes(head));
     list *max = NULL;
     list *min = NULL;
     find_limits(head, &min, &max);
-    printf("\nmax %d\nmin %d\n", max->number, min->number);
+    printf("\nsubconsecutivitatea cuprinsa intre %d is %d este:", max->number, min->number);
     list *sub_list = get_inner_list(head, min, max);
     pirnt_list(sub_list);
-    printf("total : %d nodes\n\n", count_nodes(sub_list));
+    printf("contine : %d elemente\n\n", count_nodes(sub_list));
     reverse_list(&sub_list);
+    printf("inversul acestei submultimi este: \n");
     pirnt_list(sub_list);
+    printf("\naceasta lista poate fi divizata in 2 metode:\n1. cate un element va fi trecut in fiecare lista la rand\n2. un numar de elemente va fi trecut in fiecare lista\n");
+    scanf("%d", &option);
+    node = count_nodes(sub_list);
+    printf("in cate liste doriti sa divizati? (aceata lista contine %d elemente)\n", node);
     int divide = 5;
-    list **liste = divide_in_n_lists_a(sub_list, divide);
-    for (int i = 0; i < divide; i++)
+    scanf("%d", &divide);
+    if (divide > node)
     {
-        printf("lista %d: ", i);
-        pirnt_list(liste[i]);
-        printf("total : %d nodes\n", count_nodes(liste[i]));
+        printf("nu se poate de divizat aceasta lista in %d liste", divide);
+        return 0;
     }
-    pirnt_list(sub_list);
-    liste = divide_in_n_lists_b(sub_list, divide);
-    for (int i = 0; i < divide; i++)
+    list **liste;
+    if (option == 1)
     {
-        printf("lista %d: ", i);
-        pirnt_list(liste[i]);
-        printf("total : %d nodes\n", count_nodes(liste[i]));
+        liste = divide_in_n_lists_a(sub_list, divide);
+        for (int i = 0; i < divide; i++)
+        {
+            printf("lista %d: ", i + 1);
+            pirnt_list(liste[i]);
+            printf("contine : %d elemente\n", count_nodes(liste[i]));
+        }
+    }
+    if (option == 2)
+    {
+        liste = divide_in_n_lists_b(sub_list, divide);
+        for (int i = 0; i < divide; i++)
+        {
+            printf("lista %d: ", i + 1);
+            pirnt_list(liste[i]);
+            printf("contine : %d elemente\n", count_nodes(liste[i]));
+        }
     }
     return 0;
 }
