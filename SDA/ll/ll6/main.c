254b3e017cd1dd1075a8827f99417f37fa2bcaf7
catalin <catalinhimself@gmail.com>
2021-04-05 19:25:56 +0300

    -

--- SDA/ll/ll6/main.c
+++ SDA/ll/ll6/main.c
@@ -7,6 +7,7 @@ int main()
     bin_test();
    
     tree*root=NULL; 
+
     grow_tree(&root,5);
     grow_tree(&root,3);
     grow_tree(&root,6);
@@ -15,14 +16,19 @@ int main()
    
     printf("inordine\n");
     inorder(root); 
+
     printf("preordine\n");
     preorder(root);
+
     printf("postorder\n");
     postorder(root);
     
     printf("elementul cautat\n");    
+
     print_node(search_node(root,6));    
+
     //modify_node(search_node(root,3));    
-    inorder(root);
+
+
     return 0;
 }
