331e941a9bb3970428ca9a84268cef2a83b4ac87
catalin <catalinhimself@gmail.com>
2021-04-10 00:02:14 +0300

    lab6 sda report done

--- SDA/ll/ll6/main.c
+++ SDA/ll/ll6/main.c
@@ -5,24 +5,24 @@ int main()
     srand(time(NULL));
     printf("__main__\n");
     bin_test();
-    
-    tree*root=NULL; 
-    list* tail = NULL;
-    list* head = NULL;
+
+    tree *root = NULL;
+    list *tail = NULL;
+    list *head = NULL;
 
     int order = 0;
     int option = 666;
-    while(option)
+    while (option)
     {
-    option = menu();
-    
-    switch (option)
+        option = menu();
+
+        switch (option)
         {
-        case 1:            
+        case 1:
         {
             realty temp = get_realty(++order);
             grow_tree(&root, temp);
-            break;               
+            break;
         }
         case 11:
         {
@@ -31,89 +31,92 @@ int main()
             break;
         }
         case 21:
-                if (isnt_tree_root(root))
-                    break;  
-                printf("inordine\n");
-                inorder(root); 
+            if (isnt_tree_root(root))
                 break;
-        case 22:
-                if (isnt_tree_root(root))
-                    break;  
-                printf("preordine\n");
-                preorder(root);
-                break;
-        case 23: 
-                if (isnt_tree_root(root))
-                    break;  
-                printf("postordine\n");
-                postorder(root);
-                break;
-        case 2:
-                if (isnt_tree_root(root))
-                    break;
-                append_to_queue(root, 0, &tail);
-                head = tail;
-                levels(head, &tail);
-                print_levels(root);
-                printf("numarul de noduri: %d\n",count_list_nodes(head));
-                break;
-        case 3:
-        {   
-           if (isnt_tree_root(root))
-                 break;
-            printf("elementul cautat (dupa pret)\n");    
-            int key;
-            scanf("%d",&key);
-            tree*found = search_node(root,key);
-            if (!found){
-                printf("nu exista asa nod\n");
-                break;
-            }         
-            
-            if (found->left)
-                if(found->left->immovable.price == key)
-                    found = found->left;
-            if (found->right)
-                if (found->right->immovable.price == key)
-                    found = found->right;
-            while(found->immovable.price == key){
-            put_realty(found->immovable);
-            if (found->left==NULL)
-                break;
-            found=found->left;
-            }
+            printf("inordine\n");
+            inorder(root);
             break;
-        }
-        case 4:
-        {   
-           if (isnt_tree_root(root))
-                 break;
-            printf("elementul cautat pentru modificare (dupa pret)\n");    
+        case 22:
+            if (isnt_tree_root(root))
+                break;
+            printf("preordine\n");
+            preorder(root);
+            break;
+        case 23:
+            if (isnt_tree_root(root))
+                break;
+            printf("postordine\n");
+            postorder(root);
+            break;
+        case 2:
+            if (isnt_tree_root(root))
+                break;
+            append_to_queue(root, 0, &tail);
+            head = tail;
+            levels(head, &tail);
+            print_levels(root);
+            printf("numarul de noduri: %d\n", count_list_nodes(head));
+            break;
+        case 3:
+        {
+            if (isnt_tree_root(root))
+                break;
+            printf("elementul cautat (dupa pret)\n");
             int key;
-            scanf("%d",&key);
-            tree*found = search_node(root,key);
-            if (!found){
+            scanf("%d", &key);
+            tree *found = search_node(root, key);
+            if (!found)
+            {
                 printf("nu exista asa nod\n");
                 break;
             }
 
-            tree * sub_tree = NULL; 
-            if (found->immovable.price != key)
-            {
             if (found->left)
-                if(found->left->immovable.price == key)
-                {
-                    sub_tree = found->left;
-                    found->left=NULL;
-                    found = sub_tree;
-                }
+                if (found->left->immovable.price == key)
+                    found = found->left;
             if (found->right)
                 if (found->right->immovable.price == key)
-                {
-                    sub_tree = found->right;
-                    found->right=NULL;
-                    found = sub_tree;
-                }
+                    found = found->right;
+            while (found->immovable.price == key)
+            {
+                put_realty(found->immovable);
+                if (found->left == NULL)
+                    break;
+                found = found->left;
+            }
+            break;
+        }
+        case 4:
+        {
+            if (isnt_tree_root(root))
+                break;
+            printf("elementul cautat pentru modificare (dupa pret)\n");
+            int key;
+            scanf("%d", &key);
+            tree *found = search_node(root, key);
+            if (!found)
+            {
+                printf("nu exista asa nod\n");
+                break;
+            }
+
+            tree *sub_tree = NULL;
+            if (found->immovable.price != key)
+            {
+                if (found->left)
+                    if (found->left->immovable.price == key)
+                    {
+                        sub_tree = found->left;
+                        found->left = NULL;
+                        found = sub_tree;
+                    }
+                if (found->right)
+                    if (found->right->immovable.price == key)
+                    {
+                        sub_tree = found->right;
+                        found->right = NULL;
+                        found = sub_tree;
+                    }
             }
             else
             {
@@ -121,14 +124,14 @@ int main()
                 root = NULL;
             }
 
-            while(sub_tree->immovable.price == key)
+            while (sub_tree->immovable.price == key)
             {
-                modify_node(sub_tree);    
-                if (sub_tree->left==NULL)
+                modify_node(sub_tree);
+                if (sub_tree->left == NULL)
                     break;
-                sub_tree=sub_tree->left;
+                sub_tree = sub_tree->left;
             }
-            preorder_grow(&root,found);
+            preorder_grow(&root, found);
             postorder_free_lite(&found);
             break;
         }
@@ -136,12 +139,12 @@ int main()
             append_to_queue(root, 0, &tail);
             head = tail;
             levels(head, &tail);
-            printf("numarul de noduri: %d\n",count_list_nodes(head));
+            printf("numarul de noduri: %d\n", count_list_nodes(head));
             break;
         case 6:
-           if (isnt_tree_root(root))
-               break;
-            printf("inaltimii arborelui este: %d\n",print_levels(root));
+            if (isnt_tree_root(root))
+                break;
+            printf("inaltimii arborelui este: %d\n", print_levels(root));
             break;
         case 7:
             free_list(&head);
