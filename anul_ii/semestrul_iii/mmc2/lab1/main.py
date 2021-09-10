40dc76fcc1dfbd5b76c085c63ba8dd04119777f2
Cătălin Pleșu <catalinhimself@gmail.com>
2021-09-10 09:49:13 +0300

    mmc2 lab

--- anul_ii/semestrul_iii/mmc2/lab1/main.py
+++ anul_ii/semestrul_iii/mmc2/lab1/main.py
@@ -1,28 +1,48 @@
 #!/bin/python
 from math import sqrt
-
-x0 = { 'x': 3, 'y': 3 }
-def f(x, y):
-    return 3 * x ** 2 + 3 * x * y + 3 * y ** 2 - 3 * x - 3 * y
-
-def derivata_fx(x, y):
-    return 6 * x + 3 * y - 3
-
-def derivata_fy(x, y):
-    return 3 * x + 6 * y - 3
-
-def magnitudinea(x, y):
-    return sqrt(derivata_fx(x, y) ** 2 + derivata_fy(x, y) ** 2)
+# Varianta 13
+# un punct ales (impus) arbitrar 
+# x0 = (a, b)
+a, b = (3, 3)
 
 lamda = 1
 gama = 0.5
 epsilon = 10 ** -6
 sigma = 0.06
-k = 0
+
+#functia
+def f(x, y):
+    return a * x ** 2 + 3 * x * y + b * y ** 2 - a * x - b * y
+
+# gradientul functie
+def derivata_fx(x, y):
+    return a * 2 * x + 3 * y - a
+def derivata_fy(x, y):
+    return 3 * x + b * 2 * y - b
+
+def magnitudinea(x, y):
+    return sqrt(derivata_fx(x, y) ** 2 + derivata_fy(x, y) ** 2)
 
 def z(x, y):
-    return {'x': 1, 'y':1}
+    return [ x - lamda * derivata_fx(x, y), y - lamda * derivata_fy(x, y)]
 
 if __name__ == "__main__":
-    print("functia", f(3, 3))
-    print("magnitudinea", magnitudinea(3, 3))
+    # pasul 1 gradient
+    k = 0
+    x = []
+    x.append([a, b])
+
+    #pasul 2 gr
+    while(magnitudinea(*x[k]) > epsilon):
+        print('x[',k,']',x[k],'f(x)',f(*x[k]))
+        # pasul 3 fractionarea pasului
+        while( f(*z(*x[k])) - f(*x[k]) > - sigma * lamda * magnitudinea(*x[k]) ** 2):
+            # pasul 4 fractionare
+            lamda = lamda * gama
+        # pasul 4 gr
+        x.append(z(*x[k]))
+        k += 1
+    print("STOP")
+    x = x[k]
+    print('x = ', x)
+    print('f(x*) = ', f(*x))
